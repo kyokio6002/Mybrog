a0a1394f9bfb8041eef71e1464747b5ea89599b8
kyokio6002 <kyokio0725@gmail.com>
2021-03-05 17:19:05 +0900

    add paging & -keyword_search

--- article/views.py
+++ article/views.py
@@ -1,29 +1,38 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import View
+from django.views.generic import View, ListView
 from django.db.models import Q
 from .models import Article
 # Create your views here.
 
 
-class IndexView(LoginRequiredMixin, View):
-    def get(self, request, *args, **kwargs):
-        # queryset = Article.objects.all.()
-        queryset = Article.objects.all().filter(is_public=True)
-
-        keyword = request.GET.get('keyword')
-        if keyword:
-            queryset = queryset.filter(
-                Q(title__icontains=keyword) | Q(description__icontains=keyword)
-            )
-        context = {
-            'keyword': keyword,
-            'articles': queryset,
-        }
-        return render(request, 'article/index.html', context)
+class IndexView(LoginRequiredMixin, ListView):
+    template_name = 'article/index.html'
+    context_object_name = 'articles'
+    paginate_by = 4
+    queryset = Article.objects.all().filter(is_public=True)
 
 
 index = IndexView.as_view()
+
+# class IndexView(LoginRequiredMixin, View):
+#     def get(self, request, *args, **kwargs):
+#         # queryset = Article.objects.all.()
+#         queryset = Article.objects.all().filter(is_public=True)
+# 
+#         keyword = request.GET.get('keyword')
+#         if keyword:
+#             queryset = queryset.filter(
+#                 Q(title__icontains=keyword) | Q(description__icontains=keyword)
+#             )
+#         context = {
+#             'keyword': keyword,
+#             'articles': queryset,
+#         }
+#         return render(request, 'article/index.html', context)
+# 
+# 
+# index = IndexView.as_view()
 
 class DetailView(LoginRequiredMixin, View):
     def get(self, request, article_id, *args, ** kwargs):
